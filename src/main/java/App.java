12b426f3b2fd3e2ef61a19269f41299c6f272177
Odette Ahishakiye <ahiode6@gmail.com>
2019-09-09 11:54:39 +0200

    Decrypt

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,14 +4,36 @@ import java.io.InputStreamReader;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter a word to be encrypted: ");
-        String word = bufferedReader.readLine();
-        Encryption encrypt = new Encryption();
-        System.out.println("Enter the Shift key: ");
-        String stringKey = bufferedReader.readLine();
-        int intKey = Integer.parseInt(stringKey);
-        System.out.println("The encrypted message is: "+encrypt.encryptedText(word,intKey));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
+        System.out.println("\nEncryption/Decryption of messages. Choose the action: ");
+        System.out.println("=====================================================");
+        System.out.println("1. Encrypt\n2. Decrypt\n3. Exit");
+        String choose = bufferedReader.readLine();
+        if (choose.equals("1")) {
+            System.out.println("Enter a word to be encrypted: ");
+            String word = bufferedReader.readLine();
+            Encryption encrypt = new Encryption();
+            System.out.println("Enter the Shift key: ");
+            String stringKey = bufferedReader.readLine();
+            int intKey = Integer.parseInt(stringKey);
+            System.out.println("The encrypted message is: " + encrypt.encryptedText(word, intKey));
+            System.out.println("Thank you!");
+        } else if (choose.equals("2")) {
+            System.out.println("Enter a word to be decrypted: ");
+            String ibanga = bufferedReader.readLine();
+            Decryption decr = new Decryption();
+            System.out.println("Enter the Shift key: ");
+            String stringKey = bufferedReader.readLine();
+            int intKey = Integer.parseInt(stringKey);
+            System.out.println("Your original message was: " + decr.decryptedText(ibanga, intKey));
+            System.out.println("Thank you!");
+        }
+        else if (choose.equals("3")){
+            System.out.println("Oooops! Thank you!");
+        }
+        else{
+            System.out.println("Ooooop! Invalid session");
+        }
     }
 }
--- src/main/java/Decryption.java
+++ src/main/java/Decryption.java
@@ -1,11 +1,11 @@
 public class Decryption {
     private  String text;
-    public String decryptedText(String cipherText, int key) {
+    public String decryptedText(String toDecrypt, int key) {
         String plainText = " ";
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
         int k = 0;
-        for (int i = 0; i < cipherText.length(); i++) {
-            int charPosition = alphabet.indexOf(plainText.charAt(i));
+        for (int i = 0; i < toDecrypt.length(); i++) {
+            int charPosition = alphabet.indexOf(toDecrypt.charAt(i));
             int keyVal = (charPosition - key) % 26;
             char replaceVal = alphabet.charAt(keyVal);
             plainText += replaceVal;
