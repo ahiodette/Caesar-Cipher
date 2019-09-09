22e1234f3d1ca1eead8ec0631f9f6472fd171728
Odette Ahishakiye <ahiode6@gmail.com>
2019-09-09 16:06:36 +0200

    Test

--- src/main/java/Decryption.java
+++ src/main/java/Decryption.java
@@ -1,18 +1,14 @@
 public class Decryption {
-
     public String decryptedText(String toDecrypt, int key) {
-        String plainText = " ";
+        String original = " ";
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
-//        int k = 0;
-        Encryption enc = new Encryption();
-        String cip = enc.encryptedText(toDecrypt,key);
-        System.out.println(cip);
-        for (int i = 0; i<cip.length(); i++) {
-            int charPosition = alphabet.indexOf(cip.charAt(i));
+        int k = 0;
+        for (int i = 0; i < toDecrypt.length(); i++) {
+            int charPosition = alphabet.indexOf(toDecrypt.charAt(i));
             int keyVal = (charPosition - key) % 26;
             char replaceVal = alphabet.charAt(keyVal);
-            plainText += replaceVal;
+            original += replaceVal;
         }
-        return plainText;
+        return original;
     }
 }
